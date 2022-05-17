3640b3d17d7e293fe035dfc8a65fb3d191794e6b
Chethan Yadav <rockchethan1111@gmail.com>
2022-05-17 09:38:24 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
 module.exports = {
-    token: "ODkwMjk1MDMyOTQxMjc3MTk1.YUtt3A.SE_ni01qZqtBtjl36OUzyimK_EM",
-    mongodb: "mongodb+srv://blacky:testmongo@cluster0.ssqzd.mongodb.net/test",
+    token: "BOT_TOKEN",
+    mongodb: "mongodb+srv://",
     prefix: "!",
-    
-    statcord: "statcord.com-LEQ9Z4iEyVJGgyqvyhaA",
-   
-   topgg: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODU4NzU3MTQwNzg4MDIyNSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjE5MDMxMDI4fQ.i2ingzBfyEDR9yeh1jqeN0VaF5y3jHcj6cPLXGddl_8",
-	 disbotlist: "cCZBbIf8MGcDgd6XssZhWBPXfj6y3lwfDaFpQlpAo2zSXY1SbFB785dah9Auryv4VS6kpFZw2zsMRbRkYBLlPAbyzQSqZ357rUWRBckc4GNdZ987NmtN92bVmvYrAfpO",
 	 hook: {
 		 guild: "https://discord.com/api/webhooks/912396354632966144/xCFcjvpIGlY_V0-M5LAQbup61wd0y_rxjSSTU4SEPvDvlKbZXbCVueeFYJ13A4SZtu2d",
 		 shard: "https://discord.com/api/webhooks/912394301277872218/6U4mnNvUxhiTtlZlmrVX55J3HPN4ZXZbe6Zyx_DXgezMfArbQRBtxTkhxYjl1G8BSBDu",
@@ -15,7 +10,7 @@ module.exports = {
 	 },
   
  auth: {
-		support: "https://discord.gg/bdfzqRDXd4",
+		support: "https://discord.gg/apexbot",
 	
 	},
  embeds: {
@@ -25,8 +20,8 @@ module.exports = {
   },
   
   owner: {
-		id: "491577179495333903", 
-		name: "Blacky#6618" 
+		id: "801478547893387345", 
+		name: "Raaz#2233" 
 	},
  
  	basiclang: "english",
