c1a000d4e300d02f699800bd7c597ebc13c168ec
Chethan Yadav <rockchethan1111@gmail.com>
2022-05-21 19:07:06 +0530

    Update shardReady.js

--- src/events/Client/shardReady.js
+++ src/events/Client/shardReady.js
@@ -1,12 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, event, id) => {
-  
-  const Giveaway = client.channels.cache.get(client.config.logs);
-  
-  const embed = new MessageEmbed()
-  .setDescription(`Shard #${id} Ready`)
-  .setColor('GREEN')
-  .setTimestamp()
-  Giveaway.send({embeds: [embed]})
+
+  console.log(`Shard #${id} Ready`)
+
 }
