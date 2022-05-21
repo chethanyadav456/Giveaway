db95a048edb294d62f300429e6e2fd3fcb74a142
Chethan Yadav <rockchethan1111@gmail.com>
2022-05-21 19:05:59 +0530

    Update shardDisconnect.js

--- src/events/Client/shardDisconnect.js
+++ src/events/Client/shardDisconnect.js
@@ -2,11 +2,6 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, event, id) => {
   
-  const Giveaway = client.channels.cache.get(client.config.logs);
-  
-  const embed = new MessageEmbed()
-  .setDescription(`Shard #${id} Disconnected`)
-  .setColor('RED')
-  .setTimestamp()
-  Giveaway.send({embeds: [embed]})
+  console.log(`Shard #${id} Disconnected`)
+
 }
