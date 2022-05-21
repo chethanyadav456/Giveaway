1191d5b2dadfb2d3eb4fffa6b098ac6792fc1fea
Chethan Yadav <rockchethan1111@gmail.com>
2022-05-21 19:07:25 +0530

    Update shardReconnecting.js

--- src/events/Client/shardReconnecting.js
+++ src/events/Client/shardReconnecting.js
@@ -2,11 +2,6 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, id) => {
   
-  const Giveaway = client.channels.cache.get(client.config.logs);
-  
-  const embed = new MessageEmbed()
-  .setDescription(`Shard #${id} Reconnecting`)
-  .setColor('ORANGE')
-  .setTimestamp()
-  Giveaway.send({embeds: [embed]})
+ console.log(`Shard #${id} Reconnecting`)
+
 }
