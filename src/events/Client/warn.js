9d2a341e7ce021d95a623462b2a14b9ea03a8c65
Chethan Yadav <rockchethan1111@gmail.com>
2022-05-21 19:08:01 +0530

    Update warn.js

--- src/events/Client/warn.js
+++ src/events/Client/warn.js
@@ -2,11 +2,6 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, error) => {
 
-   const Giveaway = client.channels.cache.get(client.config.logs);
+ console.log(`${error}`)
 
- const embed = new MessageEmbed()
- .setDescription(`${error}`)
- .setTimestamp()
- .setColor('RED')
- Giveaway.send({ embeds: [embed]})  
 }
