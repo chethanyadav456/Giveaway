96ff51a5d9f28217b5a9b240ca1fd691d4ca68b7
Chethan Yadav <rockchethan1111@gmail.com>
2022-05-21 19:07:41 +0530

    Update shardResume.js

--- src/events/Client/shardResume.js
+++ src/events/Client/shardResume.js
@@ -2,11 +2,6 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, replayedEvents, id) => {
 
-   const Giveaway = client.channels.cache.get(client.config.logs);
+  console.log(`Shard #${id} Resumed`)
 
-  const embed = new MessageEmbed()
-  .setDescription(`Shard #${id} Resumed`)
-  .setColor('GREEN')
-  .setTimestamp()
-  Giveaway.send({embeds: [embed]})
 }
