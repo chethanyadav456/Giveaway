7260610dd6edb2dc496e89b842f78f6f2adda58f
Chethan Yadav <rockchethan1111@gmail.com>
2022-05-21 19:05:15 +0530

    Update rateLimit.js

--- src/events/Client/rateLimit.js
+++ src/events/Client/rateLimit.js
@@ -1,12 +1,5 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = (client, rateLimitData) => {
-
- const Giveaway = client.channels.cache.get(client.config.logs);
-
- const embed = new MessageEmbed()
- .setDescription(`${rateLimitData}`)
- .setTimestamp()
- .setColor('RED')
- Giveaway.send({ embeds: [embed]})  
+console.log(rateLimitData)
 }
